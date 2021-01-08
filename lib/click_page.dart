9a7d267a5742968a438afa7b3f0928d004f00a7a
iamjamestan <jamestanyuli@gmail.com>
2021-01-08 16:05:18 +0800

    Enlarge snooze button

--- lib/click_page.dart
+++ lib/click_page.dart
@@ -15,24 +15,43 @@ class _ClickPageState extends State<ClickPage> {
   @override
   build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.green,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-                child: Text(widget.message,
-                    textAlign: TextAlign.center,
-                    style: new TextStyle(fontSize: 100.0))),
-            Center(
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('Snooze', style: TextStyle(fontSize: 20)),
-                color: Colors.green.shade800,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              widget.message,
+              textAlign: TextAlign.center,
+              style: new TextStyle(fontSize: 100.0),
+            ),
+          ),
+          Center(
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
               ),
-            )
-          ],
-        ));
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                  bottom: 20,
+                  left: 90,
+                  right: 90,
+                ),
+                child: Text(
+                  "Snooze",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 50,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
