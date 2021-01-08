a6c5fd7f10863a6cf2324a5a68711263fae2d712
nicktohzyu <34980083+nicktohzyu@users.noreply.github.com>
2021-01-08 23:33:01 +0800

    fix data persistence bug

--- lib/settings_form.dart
+++ lib/settings_form.dart
@@ -22,6 +22,8 @@ class _SettingsFormState extends State<SettingsForm> {
   final _formKey = GlobalKey<FormState>();
   final extendFocus = FocusNode();
   bool isFlashing = false;
+  String time;
+  String extendTime;
 
   //for data persistence
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
@@ -49,10 +51,14 @@ class _SettingsFormState extends State<SettingsForm> {
   }
 
   Future<Map> getPreferences() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String loadedTime = prefs.getString("time");
-    String loadedExtendTime = prefs.getString("extendTime");
-    return {"loadedTime": loadedTime, "loadedExtendTime": loadedExtendTime};
+    if(time == null || extendTime == null){
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String loadedTime = prefs.getString("time");
+      time = loadedTime == null ? "" : loadedTime;
+      String loadedExtendTime = prefs.getString("extendTime");
+      extendTime = loadedExtendTime == null ? "" : loadedExtendTime;
+    }
+    return {"time": time, "extendTime": extendTime};
   }
 
   Future<void> savePreferences() async {
@@ -91,9 +97,9 @@ class _SettingsFormState extends State<SettingsForm> {
                 future: getPreferences(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.hasData) {
-                    final _newValue = snapshot.data["loadedTime"] == null
+                    final _newValue = snapshot.data["time"] == null
                         ? ""
-                        : snapshot.data["loadedTime"];
+                        : snapshot.data["time"];
                     fromController.value = TextEditingValue(
                       text: _newValue,
                       selection: TextSelection.fromPosition(
@@ -122,6 +128,11 @@ class _SettingsFormState extends State<SettingsForm> {
                         if (n == null || n <= 0) {
                           return 'Please enter a positive number.';
                         }
+                      },
+                      onChanged: (text){
+                        setState(() {
+                          time = text;
+                        });
                       },
                       onFieldSubmitted: (_) {
                         FocusScope.of(context).requestFocus(extendFocus);
@@ -153,9 +164,9 @@ class _SettingsFormState extends State<SettingsForm> {
                 future: getPreferences(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.hasData) {
-                    final _newValue = snapshot.data["loadedExtendTime"] == null
+                    final _newValue = snapshot.data["extendTime"] == null
                         ? ""
-                        : snapshot.data["loadedExtendTime"];
+                        : snapshot.data["extendTime"];
                     extendController.value = TextEditingValue(
                       text: _newValue,
                       selection: TextSelection.fromPosition(
@@ -185,6 +196,11 @@ class _SettingsFormState extends State<SettingsForm> {
                           return 'Please enter a positive number.';
                         }
                       },
+                      onChanged: (text){
+                        setState(() {
+                          extendTime = text;
+                        });
+                      }
                     );
                   } else {
                     return Center(
